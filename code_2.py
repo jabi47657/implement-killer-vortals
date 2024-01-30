8b518f34d41af6547d31c69cfe4f1b92b59304ab
jabi47657 <146475003+jabi47657@users.noreply.github.com>
2024-01-30 18:46:06 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,29 @@
+            System.out.println("Random Number: " + item);
+
+    }
+}
+public class RandomDataGenerator {
+    public static void main(String[] args) {
+        }
+
+        }
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int item : data) {
+    }
+    const data = [];
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+    data.forEach(item => {
+}
+    });
+    return data;
+}
+    for (let i = 0; i < 10; i++) {
+
+        console.log("Random Number: " + item);
+main();
+    const data = generateRandomData();
+function generateRandomData() {
+function main() {
